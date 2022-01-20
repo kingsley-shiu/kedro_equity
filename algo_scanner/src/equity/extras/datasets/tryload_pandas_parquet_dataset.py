a4028220b0234a3e5ca5e77b18ecaf07038b0a64
kingsley-shiu <shiukingpan@gmail.com>
2022-01-20 22:30:52 +0800

    lint

--- algo_scanner/src/equity/extras/datasets/tryload_pandas_parquet_dataset.py
+++ algo_scanner/src/equity/extras/datasets/tryload_pandas_parquet_dataset.py
@@ -6,10 +6,24 @@ from kedro.io.core import Version
 
 
 class tryload_pandas_parquet(ParquetDataSet):
-    
-    def __init__(self, filepath: str, load_args: Dict[str, Any] = None, save_args: Dict[str, Any] = None, version: Version = None, credentials: Dict[str, Any] = None, fs_args: Dict[str, Any] = None) -> None:
-        super().__init__(filepath, load_args=load_args, save_args=save_args, version=version, credentials=credentials, fs_args=fs_args)
-        
+    def __init__(
+        self,
+        filepath: str,
+        load_args: Dict[str, Any] = None,
+        save_args: Dict[str, Any] = None,
+        version: Version = None,
+        credentials: Dict[str, Any] = None,
+        fs_args: Dict[str, Any] = None,
+    ) -> None:
+        super().__init__(
+            filepath,
+            load_args=load_args,
+            save_args=save_args,
+            version=version,
+            credentials=credentials,
+            fs_args=fs_args,
+        )
+
     def _load(self) -> pd.DataFrame:
         try:
             return super()._load()
--- algo_scanner/src/equity/pipelines/data_engineering/pipeline.py
+++ algo_scanner/src/equity/pipelines/data_engineering/pipeline.py
@@ -17,28 +17,28 @@ def create_pipeline(**kwargs):
                 inputs=None,
                 outputs="nasdaq_symbol",
                 name="nasdaq_symbol",
-                tags="gen"
+                tags="gen",
             ),
             node(
                 func=nodes.get_nasdaq_daily_price,
                 inputs="nasdaq_symbol",
                 outputs="nasdaq_daily_price",
                 name="nasdaq_daily_price",
-                tags="gen"
+                tags="gen",
             ),
             node(
                 func=nodes.get_nasdaq_company_info,
                 inputs=["nasdaq_symbol", "nasdaq_company_info_loaded"],
                 outputs="nasdaq_company_info",
                 name="nasdaq_company_info",
-                tags="gen"
+                tags="gen",
             ),
             node(
                 func=nodes.get_nasdaq_share_held,
                 inputs=["nasdaq_symbol", "nasdaq_share_held_loaded"],
                 outputs="nasdaq_share_held",
                 name="nasdaq_share_held",
-                tags="gen"
+                tags="gen",
             ),
         ]
     )
